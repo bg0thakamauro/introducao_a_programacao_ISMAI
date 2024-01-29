1518b6427759b089bca914d51d5b56363563a345
Mauro Marques <rmauromarques@gmail.com>
2024-01-29 04:54:25 +0000

    Update exe9.py

--- Ficha de trabalho 2/Mauro/exe9.py
+++ Ficha de trabalho 2/Mauro/exe9.py
@@ -4,7 +4,7 @@ n2 = int(input("Insira o angulo 2: "))
 n3 = n1 + n2
 
 if n3 > 180:
-    print("foi inserido algulos maiores do que era suposto")
+    print("foi inserido angulos maiores do que era suposto")
 else:
     n4 = 180 - n3
     print("O valor do 3 angulo é: ", n4)
